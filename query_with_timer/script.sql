4e79a91902b8dfa3b145be528f7731876ac7f423
Gustavo Saijo <132283787+gsaijo@users.noreply.github.com>
2024-09-17 20:39:30 -0300

    Comments

--- query_with_timer/script.sql
+++ query_with_timer/script.sql
@@ -1,5 +1,7 @@
+-- Definir o tempo inicial
 SET @start_time = NOW();
 
+-- Consulta principal
 SELECT pr.produto, SUM(ve.qty * pr.preco) AS total_venda
 FROM produtos pr 
 JOIN vendas ve
@@ -7,12 +9,13 @@ ON pr.codigo = ve.codigo
 GROUP BY pr.produto
 ORDER by total_venda DESC;
 
+-- Verificar se há vendas sem produtos correspondentes
 SELECT ve.*
 FROM vendas vendas
 LEFT JOIN produtos pr ON ve.codigo = pr.codigo
 WHERE pr.codigo IS NULL;
 
+-- Definir o tempo final
 SET @end_time = NOW();
+-- Calcular o tempo de execução
 SELECT TIMEDIFF(@end_time, @start_time) AS tempo_de_execucao;
-
-
